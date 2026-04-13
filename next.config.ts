29b17f2c994d7b3ea67833ff8f70a7dc5f681b65
efbrace <ebenezerflintwoodbrace@gmail.com>
2026-04-13 12:04:03 +0000

    fix conflict in NavItem

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
   images: {
-    formats: ['image/webp', 'image/avif'],
+    formats: ["image/webp", "image/avif"],
   },
 };
 
